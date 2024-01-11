702baa53edd118d13cedbccd71521d309d8bb299
User <user@Users-MacBook-Pro.local>
2024-01-11 14:55:35 +0100

    set up redux-toolkit

---
+++ src/store/provider.jsx
@@ -0,0 +1,12 @@
+import { Provider } from "react-redux";
+import { store } from "./index";
+
+export function ReduxProvider({ children }) {
+  return (
+    <Provider store={store}>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+      {children}
+      {/* </PersistGate> */}
+    </Provider>
+  );
+}
